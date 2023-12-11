4d899d08ae62239d81ac38d1b5346c0d2628be38
Daikoo0 <fernando11.dc2@gmail.com>
2023-12-10 22:11:23 -0300

    Estilos a inicio sesión y registron de usuarios

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {useTheme} from '../Context/theme-context';
+import { useTheme } from '../Context/theme-context';
 import { useNavigate } from "react-router-dom";
 import './Form.css';
 
@@ -7,7 +7,7 @@ function Login() {
   const [Correo, setCorreo] = useState('');
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
-  const { setTheme, availableThemes  } = useTheme();
+  const { setTheme, availableThemes } = useTheme();
   const navigate = useNavigate();
 
   const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -23,82 +23,96 @@ function Login() {
   };
 
   async function handleLogin() {
-   
+
     const response = await fetch("http://localhost:3001/users/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        credentials: "include",
-        body: JSON.stringify({
-         email : Correo,
-         password : password,
-        }),
-      });
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        email: Correo,
+        password: password,
+      }),
+    });
 
-      const data = await response.json();
-      console.log(response.status, data);
+    const data = await response.json();
+    console.log(response.status, data);
 
-      if(response.status===200){
-        setMessage("Sesión iniciada");
-        navigate('/home'); 
-      }
-      else{
-        setMessage("Usuario o contraseña incorrecta")
-      }
-   
+    if (response.status === 200) {
+      setMessage("Sesión iniciada");
+      navigate('/home');
+    }
+    else {
+      setMessage("Usuario o contraseña incorrecta")
+    }
+
   };
 
   return (
-    <div className="hero min-h-screen bg-base-200">
+    
       <div className="hero-content flex-col lg:flex-row-reverse">
-       
-         <div className="text-center lg:text-left">
-             <h1 className="text-5xl font-bold">Inicia sesión</h1>
-              <div>
-                 <label htmlFor="theme-select">Elige un tema:</label>
-                 <select id="theme-select" onChange={handleThemeChange}>
-                  {availableThemes.map(theme => (
-                      <option key={theme} value={theme}>
-                      {theme.charAt(0).toUpperCase() + theme.slice(1)}
-                      </option>
-                  ))}
-                 </select>
-              </div>
-         </div>
-      
-         <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-             <form className="card-body">
-             <div className="form-control">
-                  <label htmlFor="Correo" className="label">Correo:</label>
-                    <input
-                    type="email"
-                    id="Correo"
-                    name="Correo"
-                    value={Correo}
-                    onChange={handleUsernameChange}
-                  />
-             </div>
-        <div className="form-control">
-          <label htmlFor="password" className="label">Contraseña:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
+
+        <div className="text-center">
+          <h1 className="text-5xl font-bold">Inicia sesión</h1>
+          <div className="form-control mt-6">
+            <label className="label-text">Elige un tema:</label>
+            <select className="select select-primary w-full max-w-xs" onChange={handleThemeChange}>
+              {availableThemes.map(theme => (
+                <option key={theme} value={theme}>
+                  {theme.charAt(0).toUpperCase() + theme.slice(1)}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
-        <div className="form-control mt-6">
-        <button type="button" className="btn btn-primary" onClick={handleLogin}>
-          Iniciar Sesión
-        </button>
+
+        <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form className="card-body">
+            <div className="form-control w-4/5 max-w-xs">
+              <label className="label-text">Correo:</label>
+              <input
+                className="input input-bordered w-full max-w-xs"
+                placeholder="name@uct.cl"
+                type="email"
+                id="Correo"
+                name="Correo"
+                value={Correo}
+                onChange={handleUsernameChange}
+              />
+            </div>
+            <div className="form-control w-4/5 max-w-xs">
+              <label className="label-text">Contraseña:</label>
+              <input
+                className="input input-bordered w-full max-w-xs"
+                placeholder="••••••••"
+                type="password"
+                id="password"
+                name="password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+              <a href="#"  className="link link-primary text-left">Olvidaste la contraseña?</a>
+              
+            </div>
+            <div className="form-control mt-6">
+              <button type="button" className="btn btn-primary" onClick={handleLogin}>
+                Iniciar Sesión
+              </button>
+              <p className="mt-5 text-center text-sm">
+                Not a member?{' '}
+                <a href="/register" className="link link-primary font-semibold">
+                  Crea una nueva
+                </a>
+              </p>  
+
+            </div>
+            
+            <p>{message}</p>
+          </form>
         </div>
-        <p>{message}</p>
-      </form>
       </div>
-      </div>
-    </div>
+   
   );
 }
 
--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,13 +1,16 @@
 
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
+import { useTheme } from '../Context/theme-context';
 //import './Form.css';
 
 function Register() {
   const [Correo, setCorreo] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
   const [username, setUserName] = useState('');
   const [message, setMessage] = useState('');
+  const { setTheme, availableThemes } = useTheme();
   const navigate = useNavigate();
 
   const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -18,85 +21,139 @@ function Register() {
     setPassword(e.target.value);
   };
 
+  const handlePasswordConfirmChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPasswordConfirm(e.target.value);
+  };
+
   const handleUserNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUserName(e.target.value);
   };
 
+  const handleThemeChange = (event) => {
+    setTheme(event.target.value);
+  };
+
   async function handleLogin() {
-   
+
     const response = await fetch("http://localhost:3001/users/register", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        credentials: "include",
-        body: JSON.stringify({
-          email : Correo,
-          name : username,
-          password : password,
-        }),
-      });
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        email: Correo,
+        name: username,
+        password: password,
+        passwordConfirm: passwordConfirm,
+      }),
+    });
 
-      const data = await response.json();
+    const data = await response.json();
 
-      if(response.status===201){
-        setMessage("Usuario creado")
-        navigate('/');
-      }else
-      if(response.status===409){
+    if (response.status === 201) {
+      setMessage("Usuario creado")
+      navigate('/');
+    } else
+      if (response.status === 409) {
         setMessage("Este usuario ya existe")
-      }else
-      if(response.status===400  && data.message==="Key: 'RegisterUser.Email' Error:Field validation for 'Email' failed on the 'email' tag"){
-        setMessage("Email inválido")
-      }else
-      if(response.status===400  && data.message==="Key: 'RegisterUser.Password' Error:Field validation for 'Password' failed on the 'min' tag"){
-        setMessage("Contraseña muy corta")
-      }else 
-      if(response.status===400){
-        setMessage("Email y contraseña incorrectos")
-      }
-      console.log(response.status, data)
+      } else
+        if (response.status === 400 && data.message === "Key: 'RegisterUser.Email' Error:Field validation for 'Email' failed on the 'email' tag") {
+          setMessage("Email inválido")
+        } else
+          if (response.status === 400 && data.message === "Key: 'RegisterUser.Password' Error:Field validation for 'Password' failed on the 'min' tag") {
+            setMessage("Contraseña muy corta")
+          } else
+            if (response.status === 400) {
+              setMessage("Email y contraseña incorrectos")
+            }
+    console.log(response.status, data)
   };
 
   return (
-    <div className="login-container">
-      <h2>Registrarse</h2>
-      <form>
-        <div className="form-group">
-          <label htmlFor="Correo">Correo:</label>
-          <input
-            type="email"
-            id="Correo"
-            name="Correo"
-            value={Correo}
-            onChange={handleUsernameChange}
-          />
+    <div className="hero-content flex-col lg:flex-row-reverse">
+      
+      <div className="text-center">
+        <h1 className="text-5xl font-bold">Registrate</h1>
+        <div className="form-control mt-6">
+          <label className="label-text">Elige un tema:</label>
+          <select className="select select-primary w-full max-w-xs" onChange={handleThemeChange}>
+            {availableThemes.map(theme => (
+              <option key={theme} value={theme}>
+                {theme.charAt(0).toUpperCase() + theme.slice(1)}
+              </option>
+            ))}
+          </select>
         </div>
-        <div className="form-group">
-          <label htmlFor="password">Nombre de usuario:</label>
-          <input
-            id="password"
-            name="password"
-            value={username}
-            onChange={handleUserNameChange}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Contraseña:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </div>
-        <button type="button" onClick={handleLogin}>
-          Crear mi usuario
-        </button>
-        <p>{message}</p>
-      </form>
+      </div>
+
+      <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+        <form className="card-body">
+
+          <div className="form-control w-4/5 max-w-xs">
+            <label className="label-text">Nombre de usuario:</label>
+            <input
+              className="input input-bordered w-full max-w-xs"
+              placeholder="username"
+              id="username"
+              name="username"
+              value={username}
+              onChange={handleUserNameChange}
+            />
+          </div>
+
+          <div className="form-control w-4/5 max-w-xs">
+            <label className="label-text">Correo:</label>
+            <input
+              className="input input-bordered w-full max-w-xs"
+              placeholder="name@uct.cl"
+              type="email"
+              id="Correo"
+              name="Correo"
+              value={Correo}
+              onChange={handleUsernameChange}
+            />
+          </div>
+
+          <div className="form-control w-4/5 max-w-xs">
+            <label className="label-text">Contraseña:</label>
+      
+            <input
+              className="input input-bordered w-full max-w-xs"
+              placeholder="••••••••"
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+            <input
+              className="input input-bordered w-full max-w-xs"
+              placeholder="Confirmar contraseña"
+              type="password"
+              id="passwordConfirm"
+              name="passwordConfirm"
+              value={password}
+              onChange={handlePasswordConfirmChange}
+            />
+            
+          </div>
+          <div className="form-control mt-6">
+            <button type="button" className="btn btn-primary" onClick={handleLogin}>
+              Crear mi usuario
+            </button>
+          </div>
+          <p className="mt-5 text-center text-sm">
+            Ya estas registrado?{' '}
+            <a href="/" className="link link-primary font-semibold">
+              Inicia sesión
+            </a>
+          </p> 
+          <p>{message}</p>
+        </form>
+      </div>
     </div>
+
   );
 }
 
